5cd2d4c8ca9db596a36dc18d65ecb7d4649f2ea1
ifilipe.lype <filipe7lype@gmail.com>
2021-05-18 21:05:21 +0100

    fix typo

--- server/__tests__/fixtures/index.js
+++ server/__tests__/fixtures/index.js
@@ -5,7 +5,7 @@ function makeFakeUser(overrides){
         photo: faker.random.image(),
         name: faker.name.findName() ,
         bio: faker.random.alpha({ count: 255 }),
-        photo: faker.phone.phoneNumber(),
+        phone: faker.phone.phoneNumber(),
         email: faker.internet.exampleEmail(),
         password: "H3lov4al1ndpwD", // Hard code a valid password, due to max callstack recur error
         ...overrides
