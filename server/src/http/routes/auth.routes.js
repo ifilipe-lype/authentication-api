09688dacbeaca5b35cc9861effcde3b6027760f2
ifilipe.lype <filipe7lype@gmail.com>
2021-05-23 00:01:35 +0100

    adds signin route

--- server/src/http/routes/auth.routes.js
+++ server/src/http/routes/auth.routes.js
@@ -3,6 +3,7 @@ const makeExpressCallBack = require("../express-callback");
 function makeAuthRoutes({ router, AuthController }){
     return router
         .post("/signup", makeExpressCallBack(AuthController.postSignUp))
+        .post("/signin", makeExpressCallBack(AuthController.postSignIn))
     ;
 }
 
