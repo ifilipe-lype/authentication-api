6acbe01127f2b385d596ed8dd09cd26508a0ce9d
ifilipe.lype <filipe7lype@gmail.com>
2021-06-04 15:14:16 +0100

    adds user routes to main router

--- src/http/routes/index.js
+++ src/http/routes/index.js
@@ -1,11 +1,16 @@
 const Router = require("express").Router;
 
 const AuthController = require("../controllers/auth");
+const UserController = require("../controllers/user");
+
+const { authMiddleware } = require("../middlewares")
 
 const authRoutes = require("./auth.routes")({ router: new Router, AuthController });
+const userRoutes = require("./user.routes")({ router: new Router, UserController });
 
 const routes = new Router();
 
 routes.use("/auth", authRoutes);
+routes.use("/users", authMiddleware, userRoutes);
 
 module.exports = routes;
