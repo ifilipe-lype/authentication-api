c3ad2f13e81547c39a83395a21037188b4d99d4b
ifilipe.lype <filipe7lype@gmail.com>
2021-06-10 13:11:53 +0100

    passes AuthService to updateUser method

--- src/services/user/index.js
+++ src/services/user/index.js
@@ -1,9 +1,11 @@
 const { UserDb } = require("../../db");
 
+const AuthService = require("../auth");
+
 const makeGetProfile = require("./get-profile");
 const makeUpdateUser = require("./update-user");
 
 module.exports = Object.freeze({
     getProfile: makeGetProfile({ UserDb }),
-    updateUser: makeUpdateUser({ UserDb }),
+    updateUser: makeUpdateUser({ UserDb, AuthService }),
 });
