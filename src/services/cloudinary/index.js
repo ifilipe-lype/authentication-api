388363b1da9eb30888c4587e91c7d26577308910
ifilipe.lype <filipe7lype@gmail.com>
2021-06-12 20:11:06 +0100

    throws an AppError if err occuried during uploading

--- src/services/cloudinary/index.js
+++ src/services/cloudinary/index.js
@@ -1,5 +1,6 @@
 const { v2: cloudinary } = require("cloudinary");
 const fs = require("fs");
+const AppError = require("../../utils/AppError");
 
 cloudinary.config({
 	cloud_name: process.env.CLOUD_NAME,
@@ -7,19 +8,17 @@ cloudinary.config({
 	api_secret: process.env.CLOUDINARY_API_SECRECT
 });
 
-
-// const upload_stream = utils.promisify(cloudinary.uploader.upload_stream)({ tags: 'auth_app_photo' });
-// console.log(upload_stream)
 async function uploadWithStream(filePath){
     return new Promise((resolve, reject) => {
         const upload_stream = cloudinary.uploader.upload_stream({
             tags: 'image_upload_sample_app'
         }, (err, image) => {
             if (err){
-                return reject(new Error("Couldn't upload image file!"));
+                return reject(new AppError("Couldn't upload image file!"));
             }
             return resolve(image);
         });
+        
         fs.createReadStream(filePath).pipe(upload_stream);
     })
 
