471905d3c2a28662a464616e4d87fcc7acc5a622
ifilipe.lype <filipe7lype@gmail.com>
2021-06-13 03:29:33 +0100

    fixes empty req.body and files

--- src/http/middlewares/multipart-file-parser.js
+++ src/http/middlewares/multipart-file-parser.js
@@ -8,21 +8,25 @@ const form = formidable({
 
 module.exports = (req, res, next) => {
     const contentType = req.headers['content-type'];
-
+    
     form.onPart = (part) => {
-        if (part.mime && part.mime.match(/image\/*/)) {
-            form.handlePart(part);
-        } else if (part.mime) {
-            res.status(400).json({ error: "file type not supported!" })
+        if (part.mime && !part.mime.match(/image\/*/)) {
+            return res.status(400).json({ error: "file type not supported!" })
         }
+        form.handlePart(part);
     }
-
+    
     if (contentType && contentType.indexOf('multipart/form-data') !== -1) {
         form.parse(req, (err, fields, file) => {
             if (!err) {
                 req.body = fields; // sets the body field in the request object
                 req.file = file; // sets the file field in the request object
             }
+
+            if(err && err.message.match("maxFileSize exceeded")) {
+                return res.status(400).json({ err: "file too big, can upload only file under 5 MB"})
+            }
+
             next(); // continues to the next middleware or route handler
         })
     } else {
